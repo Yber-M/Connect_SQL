070c41fef901943b2a97963dd22f9b791333ddd2
Yber-M <ybermarlon@gmail.com>
2023-06-23 00:01:37 -0500

    Se añadió comentarios y se termino con el código para la conexion con SQLserver

--- Proyecto12/Proyecto12/Form1.Designer.cs
+++ Proyecto12/Proyecto12/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.tbxDescripcion = new System.Windows.Forms.TextBox();
+            this.tbxPrecio = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -37,24 +41,61 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(34, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(82, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Descripción:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 90);
+            this.label2.Location = new System.Drawing.Point(34, 90);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(65, 16);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Precio: S/";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(237, 79);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(94, 44);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(128, 148);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(94, 44);
+            this.btnCerrar.TabIndex = 3;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // tbxDescripcion
+            // 
+            this.tbxDescripcion.Location = new System.Drawing.Point(119, 29);
+            this.tbxDescripcion.Name = "tbxDescripcion";
+            this.tbxDescripcion.Size = new System.Drawing.Size(212, 22);
+            this.tbxDescripcion.TabIndex = 4;
+            // 
+            // tbxPrecio
+            // 
+            this.tbxPrecio.Location = new System.Drawing.Point(105, 90);
+            this.tbxPrecio.Name = "tbxPrecio";
+            this.tbxPrecio.Size = new System.Drawing.Size(126, 22);
+            this.tbxPrecio.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 323);
+            this.ClientSize = new System.Drawing.Size(356, 214);
+            this.Controls.Add(this.tbxPrecio);
+            this.Controls.Add(this.tbxDescripcion);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -68,6 +109,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.TextBox tbxDescripcion;
+        private System.Windows.Forms.TextBox tbxPrecio;
     }
 }
 
--- Proyecto12/Proyecto12/Form1.cs
+++ Proyecto12/Proyecto12/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,5 +18,37 @@ namespace Proyecto12
             InitializeComponent();
         }
 
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Cadena de conexión y abrimos la conexión a la base de datos
+            SqlConnection conexion = new SqlConnection("server = . ; database = base1 ; integrated security = true");
+
+            // Aperturamos la conexión
+            conexion.Open();
+
+            // Extraemos los datos de las cajas de texto
+            string descripcion = tbxDescripcion.Text;
+            string precio = tbxPrecio.Text;
+
+            // Crear la cadena SQL para la inserción de datos en la tabla PRODUCTO
+            string cadena = "INSERT INTO PRODUCTO(descripcion, precio) " +
+                "values('"+ descripcion +"', '"+ precio +"')";
+
+            // Crear un nuevo comando SQL utilizando la cadena y la conexión
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+
+            // Crear un nuevo comando SQL utilizando la cadena y la conexión
+            comando.ExecuteNonQuery();
+
+            // Mostrar un mensaje indicando que el proceso fue correctamente concluido
+            MessageBox.Show("Los Datos se guardaron correctamente");
+
+            // Vaciamos las cajas de texto
+            tbxDescripcion.Text = "";
+            tbxPrecio.Text = "";
+
+            // Cerramos la conexión
+            conexion.Close();
+        }
     }
 }
