defa2cd0d8a5f50ca0a80e4a5aed750dc6fe0440
Yber-M <ybermarlon@gmail.com>
2023-06-24 17:55:54 -0500

    Ejemplo 2 v0.4 GLOBTECx_Cars, formulario para registrar un carro al DB funcionando con éxito.

---
+++ GLOBTECx_Cars/GLOBTECx_Cars/FrmAddCar.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace GLOBTECx_Cars
+{
+    partial class FrmAddCar
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddCar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxPlaca = new System.Windows.Forms.TextBox();
+            this.tbxMarca = new System.Windows.Forms.TextBox();
+            this.tbxModelo = new System.Windows.Forms.TextBox();
+            this.tbxColor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAddCar
+            // 
+            this.btnAddCar.Font = new System.Drawing.Font("mononoki", 10F);
+            this.btnAddCar.Location = new System.Drawing.Point(382, 241);
+            this.btnAddCar.Name = "btnAddCar";
+            this.btnAddCar.Size = new System.Drawing.Size(291, 55);
+            this.btnAddCar.TabIndex = 0;
+            this.btnAddCar.Text = "Añadir Vehículo";
+            this.btnAddCar.UseVisualStyleBackColor = true;
+            this.btnAddCar.Click += new System.EventHandler(this.btnAddCar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("mononoki", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Placa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("mononoki", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Color";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("mononoki", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Modelo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("mononoki", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Marca";
+            // 
+            // tbxPlaca
+            // 
+            this.tbxPlaca.Font = new System.Drawing.Font("mononoki", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPlaca.Location = new System.Drawing.Point(161, 90);
+            this.tbxPlaca.MaxLength = 6;
+            this.tbxPlaca.Name = "tbxPlaca";
+            this.tbxPlaca.Size = new System.Drawing.Size(422, 38);
+            this.tbxPlaca.TabIndex = 5;
+            // 
+            // tbxMarca
+            // 
+            this.tbxMarca.Font = new System.Drawing.Font("mononoki", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxMarca.Location = new System.Drawing.Point(160, 138);
+            this.tbxMarca.MaxLength = 20;
+            this.tbxMarca.Name = "tbxMarca";
+            this.tbxMarca.Size = new System.Drawing.Size(423, 38);
+            this.tbxMarca.TabIndex = 6;
+            // 
+            // tbxModelo
+            // 
+            this.tbxModelo.Font = new System.Drawing.Font("mononoki", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxModelo.Location = new System.Drawing.Point(161, 192);
+            this.tbxModelo.MaxLength = 20;
+            this.tbxModelo.Name = "tbxModelo";
+            this.tbxModelo.Size = new System.Drawing.Size(422, 38);
+            this.tbxModelo.TabIndex = 7;
+            // 
+            // tbxColor
+            // 
+            this.tbxColor.Font = new System.Drawing.Font("mononoki", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxColor.Location = new System.Drawing.Point(160, 248);
+            this.tbxColor.MaxLength = 20;
+            this.tbxColor.Name = "tbxColor";
+            this.tbxColor.Size = new System.Drawing.Size(202, 38);
+            this.tbxColor.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("mononoki", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(196, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(459, 42);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Añadir un Nuevo Carro";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("mononoki", 10F);
+            this.btnClose.Location = new System.Drawing.Point(679, 241);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(177, 55);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label6.Location = new System.Drawing.Point(605, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(233, 152);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Recuerda que La longitud \r\nde carácacteres debe \r\nser de la siguiente forma:\r\n\r\n[" +
+    "Placa] = 6\r\n[Marca] <= 20\r\n[Modelo] <= 20\r\n[Color] <= 20";
+            // 
+            // FrmAddCar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 313);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbxColor);
+            this.Controls.Add(this.tbxModelo);
+            this.Controls.Add(this.tbxMarca);
+            this.Controls.Add(this.tbxPlaca);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddCar);
+            this.Name = "FrmAddCar";
+            this.Text = "Registrar un carro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddCar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxPlaca;
+        private System.Windows.Forms.TextBox tbxMarca;
+        private System.Windows.Forms.TextBox tbxModelo;
+        private System.Windows.Forms.TextBox tbxColor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ GLOBTECx_Cars/GLOBTECx_Cars/FrmAddCar.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GLOBTECx_Cars
+{
+    public partial class FrmAddCar : Form
+    {
+        // Cadena en la que abrimos la conexión con la base de datos
+        SqlConnection conexion = new SqlConnection("server = YB-M\\YBM ; database = GLOBTECX_CARS ; integrated security = true");
+        public FrmAddCar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAddCar_Click(object sender, EventArgs e)
+        {
+            // Aperturamos la conexión
+            conexion.Open();
+
+            // Extraemos los datos de las cajas de texto y las guardamos en variables
+            string placa = tbxPlaca.Text;
+            string marca = tbxMarca.Text;
+            string modelo = tbxModelo.Text;
+            string color = tbxColor.Text;
+
+            // Mensaje de éxito para el usuario
+            MessageBox.Show("El carro MARCA [" + marca + "] con la PLACA [" + placa + "] fue AGREGADO con ÉXITO.");
+
+            // Crear la cadena para insertar los datos a la tabla vehiculo de SQL
+            string cadena = "INSERT INTO vehiculo (Placa, Marca, Modelo, Color) VALUES " +
+                "('" + placa + "', '" + marca + "', '" + modelo + "', '" + color + "')";
+
+            // Crear un nuevo comando SQL utilizando la cadena y la conexión
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+
+            // Ejecutar el nuevo comando SQL utilizando la cadena y la conexión
+            comando.ExecuteNonQuery();
+
+            // Vaciamos las cajas de texto
+            tbxPlaca.Text = "";
+            tbxMarca.Text = "";
+            tbxModelo.Text = "";
+            tbxColor.Text = "";
+
+            // Cerramos la conexión
+            conexion.Close();
+        }
+    }
+}
